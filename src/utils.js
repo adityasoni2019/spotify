d2ba4d40ac0310166c7570d6d2c1b39f1c2fd7d9
Aditya Soni <adityasoni9727@gmail.com>
2023-09-14 01:00:26 +0530

    random

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export const clientID = "0c664075c9aa4d9a923efa983fa2516d";
-export const clientSecret = "971973d2c1ff462a897b56ba421a3730"
-export const redirectURI = "http://localhost:3000/";
-export const accessToken = "BQBViu_N2HiQPP4SQhXqSWU_7WeaFKc7aB0nI7f00PCAswZI2IhaH6kKz-k7ITdps5lQbcxc4pSeS9oecLV3Wd_BRciyYoZaJZLhEFa1iyYpkDuhMNRG_o06cRLi9Om1EvRqi5PI4Pf72ftVjfbI3Fx_PxmJ3TmnbWZITWWsYVJ44-ogpXkqhcQ9bdBr_A9Wh768VRQjaPCwKynLjo1X0e_plQUbkCieTHwM_wPZ9KS1xv_WOJRuLPVYqKKY1c6RLJeLuCqgmUaF4KmoRHgW1w1ii5xw8lbtAHoVJaT723dtDaZQ7teCTq9SQ4xvcy8Jg1CdmPsOeq-p";
-export const scopes = 'user-read-private user-read-email user-library-read user-library-modify playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative user-follow-read user-follow-modify user-top-read user-read-recently-played ugc-image-upload user-read-playback-state user-modify-playback-state user-read-playback-position user-read-currently-playing'
-export const spotifyPlaylistURI = "1t5Q8PmpWP36v61kSnxIp5"; // https://open.spotify.com/playlist/1t5Q8PmpWP36v61kSnxIp5
-
-
