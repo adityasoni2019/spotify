38c3313347b24e28c48098cc5e8c69d2c21c4acf
Aditya Soni <adityasoni9727@gmail.com>
2023-09-13 23:26:15 +0530

    able to add random songs to liked songs

--- src/Components/AddSongs.js
+++ src/Components/AddSongs.js
@@ -4,20 +4,12 @@ function AddSongs() {
 
   function handleAddSongs() {
 
-    // now, what this function does is, it'll add a song to a playlist.
-
-    // const data = { ids: ['1pP81JUiLS4CMOUOp5TQfe 5QdBAG8cpZhM3kIAQLDRN2 4wMR4fRKMTyUJG6VAsmccv 3AkdSFo7quCZ781KCqNK0T 0FqBNLXlTNimxmVRdDMj6b 1MONUudxAjEk76FJvzGhuD 4EWCNWgDS8707fNSZ1oaA5 5tuiGXDSTuAM47RQ0Z16MY 1LzNfuep1bnAUR9skqdHCK 0j35X8cTq543QDYLOyqB8W 0Qr61NXlyAeQaADO5xn3rI 3HYl5AMskJFsMGht3ljlVN 5b7OgznPJJr1vHNYGyvxau 3tva6sfOV3LDxJ9gEKQErY 7bhI6rnvaVw8qgZPn32qE0 27PmvZoffODNFW2p7ehZTQ 2ohonhg0ymv8aOlmch8KT3 6Rqn2GFlmvmV4w9Ala0I1e 5PK3ZjEOu3M7xvEpzBmAbv 4YadhxDW5xup3LMqC4VT75 44ehxt2zk84L7HttZOqH2J 2y7JqO7Jc7ErdOmxpvifRF 5zzbToGMuEli0E7RVFIhDA 38BW35ddaaYCVUvgWIiTD8 3G7tRC24Uh09Hmp1KZ7LQ2 5KVO54lVq9ivO3aZAnuk1M 6S3JlDAGk3uu3NtZbPnuhS 1d2GrPJtJXKUpW6K9OhF5x 4QkvD53COA5qLA7rQ1Pa7D 6rrKbzJGGDlSZgLphopS49 0nrRP2bk19rLc0orkWPQk2 4HlFJV71xXKIGcU3kRyttv 1qPbGZqppFwLwcBC1JQ6Vr 2KnuaZYoGzDoHiBTNYOTXG 3xKsf9qdS1CyvXSMEid6g8 5XeFesFbtLpXzIVDNQP22n 2fhOljbX79loRcdl47SFye 1Qrg8KqiBpW07V7PNxwwwL 2PCI2CW9JXsrwnaD3FGAxv 5LrN7yUQAzvthd4QujgPFr 7H0ya83CMmgFcOhw0UB6ow 4eVNSHRazoHgfYGv3AYFyZ 1Bh0UzthW8pKEnYg7v40Oa 0syIc2ocYBIKASFqifvwRz 0BYWlRpriY2teLjGs2x0eV 12e7fe2wAbGfTWoLBmbZK6 3weNRklVDqb4Rr5MhKBR3D 29U7stRjqHU6rMiS8BfaI9 4pkG0BfjNzYUaFTg4mN4BL 4ZEEcEMGjkWOfZNBcIB9yE 2w6JXokk0F8i241Y8C0Mn6 1kKxvLPfgpG5NRJ9MAmYsu 1vgVsR5rkXGP4NmUcoroJH 0KzAbK6nItSqNh8q70tb0K 7JIIY2oDPFkURYMCLJuzhe 15hJmqqEtASVXl6sM7i4UF 3UCYp1hyLw59Gw9dnyg7ck 4sf4Lq52b2iCoAgpge7a9g 0tMSssfxAL2oV8Vri0mFHE 70YTBH8vOGJNMhy6186yFm 5g5XvfePSypMK6eiVL6BI7 3BdHMOIA9B0bN53jbE5nWe 03VEWFuke6NBTcwZR1rykF 0SHa9XHwYYB1UUmgbgHuhQ 75ls0gurX68lUmMjE7QcsE 5YVuTCLDxjS8pORppZhNsf 68RI4I6SKm57SxjG06OYvN 4Y5gx8VuShNmakYGlnrUzD 6sDCgB1Ulh7aw92mk0opyd 1m8VahX3nnewYL1YR4BMBp 1DBwQlPJHqOVqXlckWk1la 5Z01UMMf7V1o0MzF86s6WJ 6wVWJl64yoTzU27EI8ep20 4AA4xt0NvJCUaJYh9SoZ1y 2NbsBSshscKIPRG3kwBbfo 6bnF93Rx87YqUBLSgjiMU8 4RI9eX7jNcdaQOJifn7t6z 5zgjD51FY7qTmlEbZenYVC 56XhZKFVFeylLeAELp6pdE 22VdIZQfgXJea34mQxlt81 4jXl6VtkFFKIt3ycUQc5LT 165cwz4wGlGz0uDBhxdKLY 5bJ1DrEM4hNCafcDd1oxHx 5w40ZYhbBMAlHYNDaVJIUu 66mwmefyBslu56TuHguHDr 5QO79kh1waicV47BqGRL3g 2sGWxjseVbAcQfWiwODrHk 5DYD4zlGiFlkpLaf2Bk8Vl 1Dr5JexwA15wmKe7Y7maA9 6V486jCRSNHh0tBAMSLwI0 1PQu8BwTrJNZxqg26E4kou 76OGwb5RA9h4FxQPT33ekc 1zBBo7indT2foQGQUoloWj 4W6gvqpSxaTipukE5521Yl 1TQXIltqoZ5XXyfCbAeSQQ 5vdp5UmvTsnMEMESIF2Ym7 652zwAiP1Wdy39NY8DSMu5 74fpNVAJrKGQuqHETi4WSt 60jI0JgMLLj1VggHJVhtRs 6rjtB7KSIiJ6v9vrACMlqF'] };
-
-    // fetch("https://api.spotify.com/v1/me/tracks?ids=0nrRP2bk19rLc0orkWPQk2", {
-    fetch("https://api.spotify.com/v1/me/tracks?ids=7ouMYWpwJ422jRcDASZB7P%2C4VqPOruhp5EdPBeR92t6lQ%2C2takcwOaAZWiXQijPHIx7B", {
+    fetch("https://api.spotify.com/v1/me/tracks?ids=5QdBAG8cpZhM3kIAQLDRN2,4wMR4fRKMTyUJG6VAsmccv,3AkdSFo7quCZ781KCqNK0T,0FqBNLXlTNimxmVRdDMj6b,1MONUudxAjEk76FJvzGhuD,4EWCNWgDS8707fNSZ1oaA5,5tuiGXDSTuAM47RQ0Z16MY,1LzNfuep1bnAUR9skqdHCK,0j35X8cTq543QDYLOyqB8W", {
       method: 'PUT',
       headers: {
         'Authorization': `Bearer ${accessToken}`,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        // ids: ["1pP81JUiLS4CMOUOp5TQfe", "5QdBAG8cpZhM3kIAQLDRN2"] // Replace 'string' with the actual track IDs you want to put
-      })
     })
       .then(response => {
         if (!response.ok) {
@@ -37,8 +29,7 @@ function AddSongs() {
       <button onClick={handleAddSongs}>Add Songs to the playlist</button>
     </>
   )
-
-
+  
 }
 
 export default AddSongs;
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const clientID = "0c664075c9aa4d9a923efa983fa2516d";
 export const clientSecret = "971973d2c1ff462a897b56ba421a3730"
 export const redirectURI = "http://localhost:3000/";
-export const accessToken = "BQAbba9LZtPlTMqxOp3mm4hx-lxylLLHTuS0iyHDh7brSXt94CoWxUEaYuMUa0uCwe7bm0HMFceRSBk7YbY8UXmhlik6lHtMa9h5aBAg3_YRH7GMvlzyogREacPGR4ZMXq8lAmfC7XFGYTXb5YhJLIesO22lOFDHOYLqHxwkE8Bhha_tmdDeQbD1Lb9Kw4qUA6UD_KATc4NyPG4AXSwVFAxGNhnG3rwzIw0j0_OC-ib2_XcuwKdDEXfP0yUVj0hu6wrpbm30T-YF35x2VZgW-UPIViIjzxuL8Fs8MlS2wUB3kg6QouhzgMU-OJ5M2fK6ZCJukGPb0ntz";
+export const accessToken = "BQC0pwXqH1ZlKGnR7sBTAwWb1E8ffDHmLIdnHKWLw2Coho2Wf0T2sixx2WGOPz8JdSvdBFPyyi1HeNLY21oqrHzeELwVuXHBmM_k9Y69JpBYWIjij87bAgZGWyVX_Y0-F1vrNr3HWdcDlkcIo9ecgDDLrL5_3BAVq5kR9CuQ3Yhxo0pnfI1u3Y0dof0mTqHaxhHJ9RoPGN4sHkS6v1LPnVqvJyVZIkpdMaIUUMMoc8okVWCC58bM_tYdFDg9UTpgL-xVQ3QeU9wZ9zxc41sPyf7QB4jclWrd7QJhWGkU9CfJsPLcUxoiVo7gQOFfXKaOFZZqzwyjTWVp";
 export const scopes = 'user-read-private user-read-email user-library-read user-library-modify playlist-read-private playlist-modify-public playlist-modify-private playlist-read-collaborative user-follow-read user-follow-modify user-top-read user-read-recently-played ugc-image-upload user-read-playback-state user-modify-playback-state user-read-playback-position user-read-currently-playing'
 export const spotifyPlaylistURI = "1t5Q8PmpWP36v61kSnxIp5"; // https://open.spotify.com/playlist/1t5Q8PmpWP36v61kSnxIp5
 
